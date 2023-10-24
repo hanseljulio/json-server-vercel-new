4f851fda4011e5488614fea740167d4504fea87b
Hansel Julio <44513085+hanseljulio@users.noreply.github.com>
2023-10-24 15:03:48 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -24,7 +24,8 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
 
-    '/auth/login?email=:email&password=:password': '/users?email=:email&password=:password'
+        '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
 server.listen(3000, () => {
